e7c00c473f2824242aafaddc210b5089a823825e
Belorina <ozcan_berrin@hotmail.com>
2021-09-21 15:18:29 +0200

    toby!

--- Assets/#Project/Scriptes/TobyManager.cs
+++ Assets/#Project/Scriptes/TobyManager.cs
@@ -9,16 +9,33 @@ public class TobyManager : MonoBehaviour
     private NavMeshAgent agent;
 
     public List<TargetPoint> targetPoints = new List<TargetPoint>();
+    private int indexNextDestination = 0;
+    private Vector3 actualDestination;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        NextDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            NextDestination();
+        }
     }
+
+    private void NextDestination()
+    {
+        actualDestination = targetPoints[indexNextDestination].GivePoint();
+        agent.SetDestination(actualDestination);
+
+        indexNextDestination++;
+        if (indexNextDestination >= targetPoints.Count) indexNextDestination = 0;
+    }
+
 }
